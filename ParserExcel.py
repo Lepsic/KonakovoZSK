a2c3e994e5783f85b773335243581770a4f5b30f
lepsick <Lepsick2003@yandex.ru>
2024-06-16 15:56:47 +0300

    Fix parser excel

--- ParserExcel.py
+++ ParserExcel.py
@@ -1,12 +1,13 @@
-import pandas as pd:
+import pandas as pd
 
 
-class PasrserExcel:
+class ParserExcel:
 
     def __init__(self, file_path: str):
         self.file = file_path
+
     def save_file(self, path: str):
         """Save file"""
+
     def read_file(self, path: str):
         """Read file"""
-
