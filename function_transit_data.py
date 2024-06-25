510c8323fd4f78428aaf6565844c8d5bbf19617a
lepsick <Lepsick2003@yandex.ru>
2024-06-25 21:15:32 +0300

    added annotation.

--- function_transit_data.py
+++ function_transit_data.py
@@ -1,4 +1,4 @@
-def transition_to_destruction(temporary_resistance: int):
+def transition_to_destruction(temporary_resistance: int) -> float:
     """Тут мы переходи от чего-то к чему-то. Но пофакту тут выполняются те
     round которые ты прописывал 3 раза"""
     return round((temporary_resistance * 1000) / 160, 1)
